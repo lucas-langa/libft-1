8cf0afa1f331623883b6ce2cd416c5b7e7f108ba
Trevor MASELEME <tmaselem@e4r11p7.wethinkcode.co.za>
2018-05-25 15:11:50 +0200

    Fly you fools

--- sources/ft_atoi.c
+++ sources/ft_atoi.c
@@ -5,36 +5,36 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmaselem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/05/24 12:40:35 by tmaselem          #+#    #+#             */
-/*   Updated: 2018/05/24 17:50:34 by tmaselem         ###   ########.fr       */
+/*   Created: 2018/05/25 07:23:37 by tmaselem          #+#    #+#             */
+/*   Updated: 2018/05/25 14:36:49 by tmaselem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/libft.h"
 
-int	ft_atoi(const char *str)
+int		ft_atoi(const char *str)
 {
-	int ctrl[3];
+	int	sign;
+	int num;
+	int i;
 
-	ctrl[0] = 0;
-	ctrl[1] = 1;
-	ctrl[2] = 1;
-	while (str[ctrl[0]] != 0)
+	i = 0;
+	sign = 1;
+	num = 0;
+	
+	while (str[i] >= 9 && str[i] <= 13)
 	{
-		if ((str[ctrl[0]] >= 9 && str[ctrl[0]] <= 13) || str[ctrl[0]] == 32)
-			ctrl[0]++;
-		if (str[ctrl[0]] == 45)
-		{
-			ctrl[1] = -1;
-			ctrl[0]++;
-		}
-		if (ft_isdigit(str[ctrl[0]]) == 1)
-		{
-			ctrl[2] = (ctrl[2] * 10) + (str[ctrl[0]] + 48);
-			ctrl[0]++;
-		}
-		if (ft_isdigit(str[ctrl[0]]) == 0)
-			return (ctrl[1] * ctrl[2]);
+		i++;
 	}
-	return (ctrl[1] * ctrl[2]);
+	if (str[i] == 45)
+	{
+		sign = -1;
+		i++;
+	}
+	while (ft_isdigit(str[i]) == 1)
+	{
+		num = (num * 10) + (str[i] - 48);
+		i++;
+	}
+	return (num * sign);
 }
--- sources/ft_putnbr.c
+++ sources/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: tmaselem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/21 12:49:48 by tmaselem          #+#    #+#             */
-/*   Updated: 2018/05/24 11:20:59 by tmaselem         ###   ########.fr       */
+/*   Updated: 2018/05/25 14:38:33 by tmaselem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,13 @@
 
 void	ft_putnbr(int num)
 {
+	if (num == –2147483648)
+		ft_putstr("–2147483648");
 	if (num < 0)
-		num = -num;
+	{
+		ft_putchar(45);
+		num *= -1;
+	}
 	if (num >= 10)
 	{
 		ft_putnbr(num / 10);
